021b353284791a49aebac3737d2afa8e8eaa2230
SUDEEP RANJAN SAHOO <sudeepranjan@iitbhilai.ac.in>
2022-11-06 00:01:42 +0530

    Update Glass-Pong.svelte

--- src/routes/informal/Glass-Pong.svelte
+++ src/routes/informal/Glass-Pong.svelte
@@ -41,9 +41,9 @@
                         <p>Event Mode- Offline</p>
                         <!-- <p>Registration Fee- Absolutely Free!</p> -->
                     </div>
-                    <div class="button-container">
+<!--                     <div class="button-container">
                         <button class="glow-on-hover">Rulebook</button>
-                    </div>
+                    </div> -->
                 </div>
             </div>
             <div class="details-container">
