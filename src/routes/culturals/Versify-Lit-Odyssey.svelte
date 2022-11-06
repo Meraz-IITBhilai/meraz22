9a2f9586b731d8d5f294e01fb56655667d78d74b
SUDEEP RANJAN SAHOO <sudeepranjan@iitbhilai.ac.in>
2022-11-06 11:02:44 +0530

    Update Versify-Lit-Odyssey.svelte

--- src/routes/culturals/Versify-Lit-Odyssey.svelte
+++ src/routes/culturals/Versify-Lit-Odyssey.svelte
@@ -11,7 +11,7 @@
 	</div>
     <div class="main-container">
         <div class="poster-container">
-            <img alt="not found" class="poster1" src="/assets/poster/poster_cult.png"  border="0">
+            <img alt="not found" class="poster1" src="/assets/poster/Versify-1.png"  border="0">
         </div>
         <div class="text-container">
             <div class="empty">
